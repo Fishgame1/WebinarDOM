e4d87769da04df723138691ac8083a7070ecbca3
Fishgame <michal.wilkowski92@gmail.com>
2019-10-03 11:14:09 +0200

    updated to match new website

--- Engine/cv_upload.py
+++ Engine/cv_upload.py
@@ -6,16 +6,16 @@ class CVUpload:
         self.driver = driver
 
     def write_name(self, text):
-        self.driver.find_element_by_xpath('//input[@type="text"]').send_keys(text)
+        self.driver.find_element_by_xpath('//*[@class="job-offers__form"]//input[@type="text"]').send_keys(text)
 
     def write_email(self, text):
-        self.driver.find_element_by_xpath('//input[@type="email"]').send_keys(text)
+        self.driver.find_element_by_xpath('//*[@class="job-offers__form"]//input[@type="email"]').send_keys(text)
 
     def write_about_your_self(self, text):
-        self.driver.find_element_by_xpath('//textarea').send_keys(text)
+        self.driver.find_element_by_xpath('//*[@class="job-offers__form"]//textarea').send_keys(text)
 
     def upload_cv(self, path):
-        self.driver.find_element_by_css_selector('input[type=file]').send_keys(path)
+        self.driver.find_element_by_css_selector('*.job-offers__form input[type="file"]').send_keys(path)
 
     def apply(self):
-        self.driver.find_element_by_xpath('//input[@type="submit"]').click()
+        self.driver.find_element_by_xpath('//*[@class="job-offers__form"]//input[@type="submit"]').click()
